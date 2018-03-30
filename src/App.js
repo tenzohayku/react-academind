1825f6371e77ef8a8cb5b569c9b61a0d91971466
tenzo hayaku <tenzohayaku@gmail.com>
2018-03-31 00:51:31 +0530

    commit git
    adding to the git

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <divf className="App">
+        <h1>Hi, I am the react app</h1>
+      </divf>
+    );
+  }
+}
+
+export default App;
+
+class Awesome extends Component(props,context,updater);
+
